5ef95ae8fc7c273f471f2476d6327873a236b003
SowmithSripadi <sowmithsripadi1@gmail.com>
2024-10-29 14:59:15 -0400

    product details dialog added

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -1,3 +1,4 @@
+const product = require("../../models/product");
 const Product = require("../../models/product");
 
 const getFilteredProducts = async (req, res) => {
@@ -52,4 +53,29 @@ const getFilteredProducts = async (req, res) => {
   }
 };
 
-module.exports = { getFilteredProducts };
+const getProductDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "product not found!",
+      });
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error occurred",
+    });
+  }
+};
+
+module.exports = { getFilteredProducts, getProductDetails };
